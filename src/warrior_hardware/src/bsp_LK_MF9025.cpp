bc6b67a70d155c42cad5d6ecfa00624e1caa3630
bodaoye <1484635116@qq.com>
2023-02-03 22:05:05 +0800

    fix(h_i):send two 9025 motors.

--- src/warrior_hardware/src/bsp_LK_MF9025.cpp
+++ src/warrior_hardware/src/bsp_LK_MF9025.cpp
@@ -12,20 +12,20 @@ void MF9025DataProcess::MF9025_speed_set(uint8_t id_shift, double speed)
     //Round up
     speed_integer = ceil(speed);
 //    RCLCPP_FATAL(rclcpp::get_logger("MF9025_speed_set"),"speed_integer %d", speed_integer);}
-    send_9025_[0].ID=0x140 + id_shift;
-    send_9025_[0].SendType=0;
-    send_9025_[0].RemoteFlag=0;
-    send_9025_[0].ExternFlag=0;
-    send_9025_[0].DataLen=8;
-    send_9025_[0].Data[0] = 0xA2;
-    send_9025_[0].Data[1] = 0x00;
-    send_9025_[0].Data[2] = 0x00;
-    send_9025_[0].Data[3] = 0x00;
+    send_9025_[id_shift - 1].ID=0x140 + id_shift;
+    send_9025_[id_shift - 1].SendType=0;
+    send_9025_[id_shift - 1].RemoteFlag=0;
+    send_9025_[id_shift - 1].ExternFlag=0;
+    send_9025_[id_shift - 1].DataLen=8;
+    send_9025_[id_shift - 1].Data[0] = 0xA2;
+    send_9025_[id_shift - 1].Data[1] = 0x00;
+    send_9025_[id_shift - 1].Data[2] = 0x00;
+    send_9025_[id_shift - 1].Data[3] = 0x00;
 
-    send_9025_[0].Data[4] = *(uint8_t *)(&speed_integer);
-    send_9025_[0].Data[5] = *((uint8_t *)(&speed_integer)+1);
-    send_9025_[0].Data[6] = *((uint8_t *)(&speed_integer)+2);
-    send_9025_[0].Data[7] = *((uint8_t *)(&speed_integer)+3);
+    send_9025_[id_shift - 1].Data[4] = *(uint8_t *)(&speed_integer);
+    send_9025_[id_shift - 1].Data[5] = *((uint8_t *)(&speed_integer)+1);
+    send_9025_[id_shift - 1].Data[6] = *((uint8_t *)(&speed_integer)+2);
+    send_9025_[id_shift - 1].Data[7] = *((uint8_t *)(&speed_integer)+3);
     // RCLCPP_FATAL(rclcpp::get_logger("MF9025_speed_set"),"id: %x", send_9025_[0].ID);
 }
 
@@ -36,20 +36,20 @@ void MF9025DataProcess::MF9025_torque_set(uint8_t id_shift, double torque)
     //Round up
     torque_integer = ceil(torque);
 //    RCLCPP_FATAL(rclcpp::get_logger("MF9025_torque_set"),"torque_integer %d", torque_integer);}
-    send_9025_[0].ID=0x140 + id_shift;
-    send_9025_[0].SendType=0;
-    send_9025_[0].RemoteFlag=0;
-    send_9025_[0].ExternFlag=0;
-    send_9025_[0].DataLen=8;
-    send_9025_[0].Data[0] = 0xA1;
-    send_9025_[0].Data[1] = 0x00;
-    send_9025_[0].Data[2] = 0x00;
-    send_9025_[0].Data[3] = 0x00;
+    send_9025_[id_shift - 1].ID=0x140 + id_shift;
+    send_9025_[id_shift - 1].SendType=0;
+    send_9025_[id_shift - 1].RemoteFlag=0;
+    send_9025_[id_shift - 1].ExternFlag=0;
+    send_9025_[id_shift - 1].DataLen=8;
+    send_9025_[id_shift - 1].Data[0] = 0xA1;
+    send_9025_[id_shift - 1].Data[1] = 0x00;
+    send_9025_[id_shift - 1].Data[2] = 0x00;
+    send_9025_[id_shift - 1].Data[3] = 0x00;
 
-    send_9025_[0].Data[4] = *(uint8_t *)(&torque_integer);
-    send_9025_[0].Data[5] = *((uint8_t *)(&torque_integer)+1);
-    send_9025_[0].Data[6] = 0x00;
-    send_9025_[0].Data[7] = 0x00;
+    send_9025_[id_shift - 1].Data[4] = *(uint8_t *)(&torque_integer);
+    send_9025_[id_shift - 1].Data[5] = *((uint8_t *)(&torque_integer)+1);
+    send_9025_[id_shift - 1].Data[6] = 0x00;
+    send_9025_[id_shift - 1].Data[7] = 0x00;
     RCLCPP_FATAL(rclcpp::get_logger("MF9025_torque_set"),"id: %x", send_9025_[0].ID);
 }
 
@@ -60,20 +60,20 @@ void MF9025DataProcess::MF9025_position_set(uint8_t id_shift, double position)
     //Round up
     position_integer = ceil(position);
 //    RCLCPP_FATAL(rclcpp::get_logger("MF9025_position_set"),"position_integer %d", speed_integer);}
-    send_9025_[0].ID=0x140 + id_shift;
-    send_9025_[0].SendType=0;
-    send_9025_[0].RemoteFlag=0;
-    send_9025_[0].ExternFlag=0;
-    send_9025_[0].DataLen=8;
-    send_9025_[0].Data[0] = 0xA3;
-    send_9025_[0].Data[1] = 0x00;
-    send_9025_[0].Data[2] = 0x00;
-    send_9025_[0].Data[3] = 0x00;
+    send_9025_[id_shift - 1].ID=0x140 + id_shift;
+    send_9025_[id_shift - 1].SendType=0;
+    send_9025_[id_shift - 1].RemoteFlag=0;
+    send_9025_[id_shift - 1].ExternFlag=0;
+    send_9025_[id_shift - 1].DataLen=8;
+    send_9025_[id_shift - 1].Data[0] = 0xA3;
+    send_9025_[id_shift - 1].Data[1] = 0x00;
+    send_9025_[id_shift - 1].Data[2] = 0x00;
+    send_9025_[id_shift - 1].Data[3] = 0x00;
 
-    send_9025_[0].Data[4] = *(uint8_t *)(&position_integer);
-    send_9025_[0].Data[5] = *((uint8_t *)(&position_integer)+1);
-    send_9025_[0].Data[6] = *((uint8_t *)(&position_integer)+2);
-    send_9025_[0].Data[7] = *((uint8_t *)(&position_integer)+3);
+    send_9025_[id_shift - 1].Data[4] = *(uint8_t *)(&position_integer);
+    send_9025_[id_shift - 1].Data[5] = *((uint8_t *)(&position_integer)+1);
+    send_9025_[id_shift - 1].Data[6] = *((uint8_t *)(&position_integer)+2);
+    send_9025_[id_shift - 1].Data[7] = *((uint8_t *)(&position_integer)+3);
     RCLCPP_FATAL(rclcpp::get_logger("MF9025_position_set"),"id: %x", send_9025_[0].ID);
 }
 
